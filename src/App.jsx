2c09b1e083ac0919d2fb9fcd3c41642f20ac5f7f
DuvanJC <djcmnhoo@gmail.com>
2023-02-15 23:40:15 -0500

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <div className=''>
         <div className=" flex items-center justify-center w-full md:w-500">
           <div className="  border-t border-gray-200"></div>
-          <div className=" flex bg-white border-2 border-gray-300 shadow-lg flex  justify-center md:w-500 flex flex-col md:flex-row p-5 md:w-8/12 lg:w-6/12  flex justify-center items-start h-full">
+          <div className=" flex bg-white border-2 border-gray-300 shadow-lg justify-center md:w-500 flex flex-col md:flex-row p-5 md:w-8/12 lg:w-6/12  flex justify-center items-start h-full">
             <div className="w-full md:w-1/2 pr-4">
               <Products />
             </div>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -25,22 +25,22 @@ const Store = () => {
             <br />
             <div className="flex md:flex-row">
                 <div className="w-full md:w-1/3 p-1">
-                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 1" className="border border-gray-300 w-full h-34 object-cover" />
-                    <img onClick={() => handleIncreaseCereal()} src="cereal.png" alt="img 2" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 3" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 4" className="border border-gray-300 w-full h-48 mt-2 object-cover" />
+                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 1" className="border border-gray-300 hover:border-purple-600 shadow-lg" />
+                    <img onClick={() => handleIncreaseCereal()} src="cereal.png" alt="img 2" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 3" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 4" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
                 </div>
                 <div className="w-full md:w-1/3 p-1">
-                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 5" className="border border-gray-300 w-full h-48 object-cover" />
-                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 6" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 7" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 8" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
+                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 5" className="border border-gray-300 hover:border-purple-600 shadow-lg" />
+                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 6" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 7" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 8" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
                 </div>
                 <div className="w-full md:w-1/3 p-1">
-                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 9" className="border border-gray-300 w-full h-34 object-cover" />
-                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 10" className="border border-gray-300 w-full h-48 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseCereal()} src="cereal.png" alt="img 11" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
-                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 12" className="border border-gray-300 w-full h-34 mt-2 object-cover" />
+                    <img onClick={() => handleIncreaseMilo()} src="milo.png" alt="img 9" className="border border-gray-300 hover:border-purple-600 shadow-lg" />
+                    <img onClick={() => handleIncreaseChips()} src="chips.png" alt="img 10" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseCereal()} src="cereal.png" alt="img 11" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
+                    <img onClick={() => handleIncreaseMilk()} src="milk.png" alt="img 12" className="border border-gray-300 hover:border-purple-600 shadow-lg mt-2" />
                 </div>
             </div>
         </>
